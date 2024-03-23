dfca8a139467cc20eca662b86b1961a6b21a4218
Ishan <146906485+ishanroy16@users.noreply.github.com>
2024-03-23 20:26:09 +0600

    Add files via upload
    added week2 day2

---
+++ Week-02/day2/A_Plus_or_Minus.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b==c)
+        {
+            cout<<"+"<<"\n";
+        }
+        else if(a-b==c)
+        {
+            cout<<"-"<<"\n";
+        }
+    }
+    return 0;
+}
